f00def255673fed31f00f0288cb4df4f5c6f76b4
Izzy Hassall <Izzy.Hassall@jncc.gov.uk>
2020-01-16 14:42:03 +0000

    Added diversity by region and species

--- FPPH_THRS_AgeDiv.R
+++ FPPH_THRS_AgeDiv.R
@@ -1,17 +1,27 @@
-###Age Diversity###
-##Date created: 2020-01-15
-##Datasets used:"NFI_Prelim_BL_Ash_Tables" from NFI
+##Script Name:FPPH_THRS_AgeDiv
+##Author: Izzy Hassall, Ecosystems Analysis
+##Contact (if different from above):
+##Date Created: 2020-01-15
+##Date Modified:
+##Licence:
+##Abstract: Age diversity calculations based on National Forest Inventory data and mock datasets
 
+
+##R version 3.6.1 (2019-07-05)
+##Dependencies:
 library(readxl)
 library(tidyr)
 library(ggplot2)
 library(plyr)
 library(dplyr)
 library(vegan)
+library(reshape)
+
+##Datasets used:"NFI_Prelim_BL_Ash_Tables" from NFI and "Ch1_Woodland_FS2019"
 
 #########################################
-##READ IN AND ARRANGE NATIONAL DATASET##
 ##BROADLEAVES##
+##Using "NFI_Prelim_BL_Ash_Tables" count data for broadleaves species
 ##Values are number of trees (millions)
 #Read in excel doc and select England area
 eng_bl_age<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/National Forest Inventory England/Inventory reports/NFI_Prelim_BL_Ash_Tables.xls",sheet="Table 11",range="B5:F15")
@@ -34,15 +44,28 @@ bl_age_total<-t(bl_age_total)
 colnames(bl_age_total)<-as.character(unlist(bl_age_total[1,]))
 bl_age_total=bl_age_total[-1,]
 bl_age_total<-sapply(bl_age_total,as.numeric)
-bl_age_total
+bl_age_total<-as.data.frame(bl_age_total)
+names(bl_age_total)[names(bl_age_total)=="bl_age_total"]<-"TotalArea"
 #Calculate Shannon diversity on age data
 bl_age_div<-diversity(bl_age_total)
 bl_age_div
 
+#Pie chart for broadleaves England
+#Recreate dataframe for total area of woodland
+bl_total<-eng_bl_age[,c(1,4)]
+sapply(bl_total,class)
+bl_total$AgeClass<-as.factor(bl_total$AgeClass)
+bl_total$AgeClass<-factor(bl_total$AgeClass,levels=c("0-10","11-20","21-40","41-60","61-80","81-100","100+"))
+ggplot(bl_total,aes(x="",y=Total,fill=AgeClass))+
+  geom_bar(width=1,stat="identity")+
+  coord_polar("y",start=0)+
+  scale_fill_brewer(palette="GnBu")+
+  theme_void()
+
 #Create mock dataset to illustrate change in age diversity over time
 #In number of trees (millions)
 #Starting point the Total column of Table 11 in "NFI_Prelim_BL_Ash_Tables" 
-age_mock<-matrix(c(21.38185214,22.03419,22.4561,22.1245,22.98512,23.1431,22.8791,23.4512,
+age_bl_mock<-matrix(c(21.38185214,22.03419,22.4561,22.1245,22.98512,23.1431,22.8791,23.4512,
                   219.1835626,220.0045,220.5623,220.90852,221.451,221.00532,221.3412,220.875,
                   353.4146369,352.9899,352.7541,352.6514,352.8975,352.2514,351.99765,351.2351,
                   190.1221531,190.0561,189.8796,189.7786,189.4235,189.7869,189.5412,188.9021,
@@ -54,7 +77,7 @@ age_mock<-matrix(c(21.38185214,22.03419,22.4561,22.1245,22.98512,23.1431,22.8791
                                c("0-10","11-20","21-40","41-60","61-80","81-100","100+")))
 #Calculate Shannon diversity
 blocks<-c("2009-2014","2015-2020","2021-2025","2026-2030","2031-2035","2036-2040","2041-2045","2046-2050")
-age_div<-diversity(age_mock)
+age_div<-diversity(age_bl_mock)
 age_div
 age_div<-data.frame(blocks,age_div)
 names(age_div)[names(age_div)=="age_div"]<-"Shannon"
@@ -68,6 +91,72 @@ ggplot(data=age_div,aes(x=blocks,y=Shannon))+
   theme(axis.text.x=element_text(angle=50,hjust=1))+
   theme(axis.text.x=element_text(colour="black"))+
   theme(axis.text.y=element_text(colour="black"))
+
+##SHANNON DIVERSITY FOR REGIONS
+eng_age_reg<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/National Forest Inventory England/Inventory reports/NFI_Prelim_BL_Ash_Tables.xls",sheet="Table A.8",range="B5:F78")
+
+#Change Column names
+names(eng_age_reg)[names(eng_age_reg)=="Principal species"]<-"Species"
+names(eng_age_reg)[names(eng_age_reg)=="Private sector"]<-"Private"
+names(eng_age_reg)[names(eng_age_reg)=="Age class (years)"]<-"AgeClass"
+colnames(eng_age_reg)
+
+#Create region column
+a<-rep("NW England",times=10)
+b<-rep("NE England",times=9)
+c<-rep("Yorkshire and Humber",times=9)
+d<-rep("E Midlands",times=9)
+e<-rep("E England",times=9)
+f<-rep("SE and London",times=9)
+g<-rep("SW England",times=9)
+h<-rep("W Midlands",times=9)
+regions<-c(a,b,c,d,e,f,g,h)
+regions
+eng_age_reg$Region<-regions
+#Remove irrelevant columns and rows
+eng_age_reg<-eng_age_reg[-c(1,2,10,11,19,20,28,29,37,38,46,47,55,56,64,65,73),][,-4]
+eng_age_reg<-eng_age_reg[c(5,1,2,3,4)]
+#Specify columns as numeric
+sapply(eng_age_reg,class)
+eng_age_reg$Total<-as.numeric(eng_age_reg$Total)
+eng_age_reg$Private<-as.numeric(eng_age_reg$Private)
+eng_age_reg$FC<-as.numeric(eng_age_reg$FC)
+eng_age_reg$AgeClass<-as.factor(eng_age_reg$AgeClass)
+eng_age_reg$AgeClass<-factor(eng_age_reg$AgeClass,levels=c("0-10","11-20","21-40","41-60","61-80","81-100","100+"))
+sapply(eng_age_reg,class)
+#Calculate Shannon diversity on total broadleaves
+#Create dataframe with only total trees
+age_reg_total<-eng_age_reg[,-c(3,4)]
+sapply(age_reg_total,class)
+#Restructure data using the Reshape Package
+melt<-melt(age_reg_total,id=(c("Region","AgeClass")))
+age_reg_total<-cast(melt,Region~variable+AgeClass)
+#Rename columns to remove "Total"
+colnames(age_reg_total)<-sub("Total_","",colnames(age_reg_total))
+colnames(age_reg_total)
+#Reorder rows in NFI data order
+age_reg_total<-age_reg_total[c(4,3,8,2,1,5,6,7),]
+#Remove the Region column
+age_reg_total<-age_reg_total[,-1]
+#Set as data matrix
+age_reg_total<-data.matrix(age_reg_total)
+#Calculate Shannon H
+age_reg_H<-diversity(age_reg_total)
+age_reg_H
+#Create table showing region and Shannon H
+regions<-c("NW England","NE England","Yorkshire and Humber","E Midlands","E England","SE and London","SW England","W Midlands")
+regions
+age_reg_H<-data.frame(regions,age_reg_H)
+#Rename columns
+names(age_reg_H)[names(age_reg_H)=="age_reg_H"]<-"Shannon"
+#Visualise as barplot with rescaled y axis to show small differences
+ggplot(data=age_reg_H,aes(x=regions,y=Shannon))+
+  geom_bar(stat="identity",width=0.75,colour="black",fill="grey87")+
+  labs(y="Shannon H")+
+  coord_cartesian(ylim=c(1.4,1.7))+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
 
 ########################################
 ##Stocked area data for both broadleaves and conifer species
@@ -89,6 +178,18 @@ age_area_con<-age_area_con[c(3,1,2)]
 age_area_con<-filter(age_area_con,Ownership %in% c("Total"))
 age_area_con<-age_area_con[,-1]
 
+#Pie chart for conifers England
+#Ensure age classes are factor in correct order
+sapply(age_area_con,class)
+age_area_con$AgeClass<-as.factor(age_area_con$AgeClass)
+age_area_con$AgeClass<-factor(age_area_con$AgeClass,levels=c("0-20","21-40","41-60","61-80","81-100","100+"))
+ggplot(age_area_con,aes(x="",y=Area,fill=AgeClass))+
+  geom_bar(width=1,stat="identity")+
+  coord_polar("y",start=0)+
+  scale_fill_brewer(palette="GnBu")+
+  theme_void()
+
+############################################
 ##Broadleaves
 age_area_bl<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/National Forest Inventory England/Inventory reports/Ch1_Woodland_FS2019.xlsx",sheet="Table 1.7",range="A5:C29")
 #Tidy it up
@@ -104,7 +205,18 @@ age_area_bl<-age_area_bl[c(3,1,2)]
 #Dataframe of total woodland area
 age_area_bl<-filter(age_area_bl,Ownership %in% c("Total"))
 age_area_bl<-age_area_bl[,-1]
+#Pie chart for broadleaves England
+sapply(age_area_bl,class)
+age_area_bl$AgeClass<-as.factor(age_area_bl$AgeClass)
+age_area_bl$AgeClass<-factor(age_area_bl$AgeClass,levels=c("0-20","21-40","41-60","61-80","81-100","100+"))
+ggplot(age_area_bl,aes(x="",y=Area,fill=AgeClass))+
+  geom_bar(width=1,stat="identity")+
+  coord_polar("y",start=0)+
+  scale_fill_brewer(palette="GnBu")+
+  theme_void()
 
+##########################################
+#All trees
 #Combine to one dataframe with conifers and broadleaves
 age_all<-cbind(age_area_con,age_area_bl)
 age_all<-age_all[,-3]
@@ -139,3 +251,54 @@ ggplot(data=age_all_div,aes(x=years,y=Shannon))+
   theme(axis.text.x=element_text(angle=50,hjust=1))+
   theme(axis.text.x=element_text(colour="black"))+
   theme(axis.text.y=element_text(colour="black"))
+
+#Create barplot showing the total area for each age class woodland type
+#Create dataframe with woodland information
+age_woodland<-rbind(age_area_con,age_area_bl)
+con<-rep("Conifers",times=6)
+bl<-rep("Broadleaves",times=6)
+age_woodland$Woodland<-c(con,bl)
+ggplot(data=age_woodland,aes(x=AgeClass,y=Area,fill=Woodland))+
+  labs(y="Total Area (thousand ha)")+
+  geom_bar(stat="identity",position="dodge")+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  theme(axis.text.x=element_text(angle=60,hjust=1))+
+  theme(axis.title.x=element_blank())
+
+#########################################
+##Calculate Shannon diversity for age class and broadleaves species for England
+##Using "NFI_GB_Report_BL_StandingVolume_by_AgeClass" data for standing volume of broadleaved species (thousand m3)
+age_spec<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/National Forest Inventory England/Inventory reports/NFI_GB_Report_BL_StandingVolume_by_AgeClass.xls",sheet="Table 2 - BL by Country and GB",range="A3:N14")
+##Neaten dataset
+age_spec<-age_spec[-c(1,2,3,11),][,-c(2,3)]
+names(age_spec)[names(age_spec)=="...1"]<-"AgeClass"
+#Specify columns as numeric/factor
+sapply(age_spec,class)
+species<-c("Oak","Beech","Sycamore","Ash","Birches","Sweet chestnut","Hazel","Hawthorn","Alder","Willow","Other broadleaves")
+age_spec[species]<-sapply(age_spec[species],as.numeric)
+age_spec$AgeClass<-as.factor(age_spec$AgeClass)
+age_spec$AgeClass<-gsub(" years","",age_spec$AgeClass)
+age_spec$AgeClass<-factor(age_spec$AgeClass,levels=c("0-10","11-20","21-40","41-60","61-80","81-100","100+"))
+sapply(age_spec,class)
+#Remember age classes
+age<-age_spec$AgeClass
+#Transpose dataset without AgeClass column
+age_spec_t<-as.data.frame(t(age_spec[,-1]))
+#Set column names
+colnames(age_spec_t)<-age
+#Check column classes
+sapply(age_spec_t,class)
+#Calculate Shannon 
+age_spec_H<-diversity(age_spec_t)
+age_spec_H<-data.frame(species,age_spec_H)
+names(age_spec_H)[names(age_spec_H)=="age_spec_H"]<-"Shannon"
+#Visualise as barplot with rescaled y axis to show small differences
+ggplot(data=age_spec_H,aes(x=species,y=Shannon))+
+  geom_bar(stat="identity",width=0.75,colour="black",fill="grey87")+
+  labs(x="",y="Shannon H")+
+  coord_cartesian(ylim=c(1.3,1.7))+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
+
