48388cffb37b12c245866a59d639898125323ef5
Izzy Hassall <Izzy.Hassall@jncc.gov.uk>
2020-02-04 11:28:22 +0000

    Add mock data analysis

--- FPPH_THRS_StructuralDiv_LiDAR.R
+++ FPPH_THRS_StructuralDiv_LiDAR.R
@@ -63,24 +63,34 @@ ggplot(prof2_melt,aes(x=Distance,y=Height,colour=Year))+
 
 #First square SD38ne
 #Read in excel file saved from the shapefile
-chm<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/EA LiDAR_NWArea/CHM/NFI_Clipped_SD38ne_CHMExtent.xlsx")
+chm_38ne<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/EA LiDAR_NWArea/CHM/NFI_Clipped_SD38ne_CHMExtent.xlsx")
 #Remove non woodland subregions by selecting only woodland rows
-chm<-chm%>%filter(CATEGORY%in%c("Woodland"))
+chm_38ne<-chm_38ne%>%filter(CATEGORY%in%c("Woodland"))
 
 #Create subset of just the broadleaves and conifers stands
-chm_subs<-chm%>%filter(IFT_IOA%in%c("Broadleaved","Conifer"))
-colnames(chm_subs)
-colnames(chm_subs)<-sub("_","",colnames(chm_subs))
+chm_38ne_subs<-chm_38ne%>%filter(IFT_IOA%in%c("Broadleaved","Conifer"))
+colnames(chm_38ne_subs)
+colnames(chm_38ne_subs)<-sub("_","",colnames(chm_38ne_subs))
 #New column with object number
-chm_subs$Object<-1:nrow(chm_subs)
-ggplot(chm_subs,aes(x=Object,y=mean))+
+chm_38ne_subs$Object<-1:nrow(chm_38ne_subs)
+ggplot(chm_38ne_subs,aes(x=Object,y=mean))+
   geom_bar(stat="identity")+
   geom_errorbar(aes(ymin=mean-stdev,ymax=mean+stdev),width=0.2)+
   theme_bw()+
   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+
+#Plot line with error ribbon instead of bar chart
+ggplot(chm_38ne_subs,aes(x=Object,y=mean))+
+  geom_ribbon(aes(ymin=mean-stdev,ymax=mean+stdev),alpha=0.2)+
+  geom_line()+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  xlab("Forest Compartment")+
+  ylab("Mean Height (m)")
+
 #Table detailing mean and SD of canopy height
-colnames(chm_subs)
-chm_38ne<-chm_subs%>%select(IFTIOA,mean,median,stdev,max)
+colnames(chm_38ne_subs)
+chm_38ne<-chm_38ne_subs%>%select(IFTIOA,mean,median,stdev,max)
 
 #Generate mean and SD of the square
 mean(chm_38ne$mean)
@@ -89,6 +99,7 @@ sd(chm_38ne$mean)
 cov<-(sd(chm_38ne$mean)/mean(chm_38ne$mean))*100
 cov
 
+################################################################
 #Create mock data of mean and sd over time
 years<-c(seq(2020,2050,1))
 mock_38ne<-matrix(c(2.371,2.403,2.407,2.435,2.652,2.782,2.783,2.784,2.895,2.987,2.896,3.002,3.233,3.247,3.287,3.298,	3.312,3.314,3.354,3.445,3.502,3.525,3.562,3.451,3.451,3.489,3.561,3.572,3.689,3.672,3.680,
@@ -110,6 +121,26 @@ ggplot(data=mock_38ne,aes(x=Year,y=Mean))+
   theme_bw()+
   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
 
+#Edit mock data to show more variation in SD
+mock_38ne_2<-matrix(c(2.371,2.403,2.407,2.435,2.652,2.782,2.783,2.784,2.895,2.987,2.896,3.002,3.233,3.247,3.287,3.298,	3.312,3.314,3.354,3.445,3.502,3.525,3.562,3.451,3.451,3.489,3.561,3.572,3.689,3.672,3.680,
+                    2.732,2.720,2.611,2.561,2.312,2.132,1.967,1.823,1.897,1.652,1.512,1.324,1.234,1.432,1.678,1.783,1.892,2.001,2.34,2.462,2.354,2.132,2.563,2.679,2.451,2.562,2.586,2.566,2.671,2.581,2.643),
+                  nrow=31,
+                  dimnames=list(c(years),
+                                c("Mean","SD")))
+#Add year as column
+mock_38ne_2<-as.data.frame(mock_38ne_2)
+mock_38ne_2$Year<-years
+#Calculate Coefficient of Variation
+sapply(mock_38ne_2,class)
+mock_38ne_2$cov<-(mock_38ne_2$Mean/mock_38ne_2$SD)*100
+#Plot this edited data over time as line with shaded SD region - mock data
+ggplot(data=mock_38ne_2,aes(x=Year,y=Mean))+
+  geom_ribbon(aes(ymin=Mean-SD,ymax=Mean+SD),alpha=0.2)+
+  geom_line()+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+
+#########################################################################
 #Plot the mean with coefficient of variation shaded ribbon
 ggplot(data=mock_38ne,aes(x=Year,y=Mean))+
   geom_ribbon(aes(ymin=Mean-cov,ymax=Mean+cov),alpha=0.2)+
@@ -120,4 +151,74 @@ ggplot(data=mock_38ne,aes(x=Year,y=Mean))+
 
 #########################################################################
 #Second square SD39se 
+chm_39se<-read_excel("J:/GISprojects/Ecosystems Analysis/FPPH/THRS indicators/Data/EA LiDAR_NWArea/CHM/NFI_Clipped_SD39se_CHMExtent.xlsx")
+#Remove non woodland subregions by selecting only woodland rows
+chm_39se<-chm_39se%>%filter(CATEGORY%in%c("Woodland"))
 
+#Create subset removing Felled and Windblow areas
+chm_39se_subs<-chm_39se%>%filter(IFT_IOA%in%c("Broadleaved","Conifer","Mixed mainly broadleaved","Mixed mainly conifer","Young trees"))
+colnames(chm_39se_subs)
+colnames(chm_39se_subs)<-sub("_","",colnames(chm_39se_subs))
+
+#New column with object number
+chm_39se_subs$Object<-1:nrow(chm_39se_subs)
+ggplot(chm_39se_subs,aes(x=Object,y=mean))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=mean-stdev,ymax=mean+stdev),width=0.2)+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+
+#Plot line with error ribbon instead of bar chart
+ggplot(chm_39se_subs,aes(x=Object,y=mean))+
+  geom_ribbon(aes(ymin=mean-stdev,ymax=mean+stdev),alpha=0.2)+
+  geom_line()+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  xlab("Forest Compartment")+
+  ylab("Mean Height (m)")
+  
+#Table detailing mean and SD of canopy height
+colnames(chm_39se_subs)
+chm_39se_subs<-chm_39se_subs%>%select(IFTIOA,mean,median,stdev,max)
+
+#Generate mean and SD of the entire square
+mean(chm_39se_subs$mean)
+sd(chm_39se_subs$mean)
+#Generate coefficient of variation
+cov<-(sd(chm_39se_subs$mean)/mean(chm_39se_subs$mean))*100
+cov
+
+############################################################################
+#Generate mock data showing regional canopy height MEAN, SD, COV plotted as bar chart
+#Could plot on map??
+ch_reg<-matrix(c(3.363,2.146,4.521,2.461,3.784,3.489,2.576,3.169,
+                 2.934,1.811,2.121,1.343,1.455,1.034,1.992,2.156,
+                 114.622,92.860,213.154,183.246,154.134,171.534,129.317,146.985),
+               nrow=8,
+               dimnames=list(c("NW England","NE England","Yorkshire and Humber","E Midlands","E England","SE and London","SW England","W Midlands"),
+                             c("Mean","SD","CoV")))
+regions<-c("NW England","NE England","Yorkshire and Humber","E Midlands","E England","SE and London","SW England","W Midlands")
+ch_reg<-as.data.frame(ch_reg)
+ch_reg$Region<-c(regions)
+sapply(ch_reg,class)
+ch_reg$Region<-as.factor(ch_reg$Region)
+sapply(ch_reg,class)
+ch_reg$Region<-factor(ch_reg$Region,levels=c("NW England","NE England","Yorkshire and Humber","E Midlands","E England","SE and London","SW England","W Midlands"))
+
+#Plot as barplot
+ggplot(ch_reg,aes(x=Region,y=Mean))+
+  geom_bar(stat="identity",fill="grey")+
+  geom_errorbar(aes(ymin=Mean-SD,ymax=Mean+SD),width=0.2)+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  ylab("Mean Height (m)")+
+  xlab("")+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
+
+#Trying to change plot parameters
+par()
+opar<-par()
+#$pin
+#[1] 7.791250 3.868333
+par(pin=c(5,4))
+par(mai=c(2,2,2,2))
